d4139ecbfe07d6430a14079246a6ea42b21e596e
melkassimi <moncif1.elkassimi@gmail.com>
2020-03-14 00:01:59 +0100

    edit file

--- pyblog/settings.py
+++ pyblog/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'pyblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql', # on utilise l'adaptateur postgresql
+        'NAME': 'disquaire', # le nom de notre base de données créée précédemment
+        'USER': 'melkassimi', # attention : remplacez par votre nom d'utilisateur !!
+        'PASSWORD': 'Art010203*',
+        'HOST': '',
+        'PORT': '5432',
     }
 }
 
